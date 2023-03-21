f0a14f4e0fe45d6ed89a70e6cf7d9e080f456167
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-03-20 21:21:27 -0500

    MathController added

--- src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
+++ src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
@@ -1,5 +1,6 @@
 package com.codeup.codeupspringblog;
 
+import com.codeup.codeupspringblog.controllers.MathController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

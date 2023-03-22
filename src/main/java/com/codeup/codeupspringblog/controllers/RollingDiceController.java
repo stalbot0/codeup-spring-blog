fe544ab5190210d7d78ea86b9a7a5c3250b8a3b4
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-03-21 22:08:29 -0500

    redirect not working

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/RollingDiceController.java
@@ -0,0 +1,39 @@
+package com.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Random;
+
+@Controller
+@RequestMapping("/roll-dice")
+public class RollingDiceController {
+
+    @GetMapping
+    public String viewDiceRollForm() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/{n}")
+    public String rollNDie(@PathVariable int n, Model model) {
+        model.addAttribute("n", n);
+
+        Random x = new Random();
+        int randomNum = x.nextInt(1, 7);
+        model.addAttribute("randomDie", randomNum);
+
+//        String dieMessage;
+//        if(n == randomNum) {
+//            dieMessage = String.format("You guessed correctly. You guessed %d, I rolled a %d", n, randomNum);
+//        } else {
+//            dieMessage = String.format("You guessed incorrectly. You guessed %d, I rolled a %d", n, randomNum);
+//        }
+//        model.addAttribute("dieMessage", dieMessage);
+
+        return "roll-dice-result";
+    }
+
+}
